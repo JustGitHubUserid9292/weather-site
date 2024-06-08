f831d76899b0af77daadea4110951da27db4ab3f
JustGitHubUserid9292 <118734378+JustGitHubUserid9292@users.noreply.github.com>
2024-06-09 00:20:31 +0300

    rabota idet =)

--- src/app.js
+++ src/app.js
@@ -8,16 +8,22 @@ export default function app() {
         btn.addEventListener('click', () => {
             const infoReplace = {
                 city: btn.querySelector('h2').innerHTML,
-                weather: btn.querySelector('p').innerHTML
+                weather: btn.querySelector('p').innerHTML,
+                additional_info: btn.querySelector('.additional-info').innerHTML
             };
             const mainInfo = {
                 city: mainCityCard.querySelector('h1').innerHTML,
-                weather: mainCityCard.querySelector('p').innerHTML
+                weather: mainCityCard.querySelector('p').innerHTML,
+                additional_info: mainCityCard.querySelector('.additional-info').innerHTML
             };
             mainCityCard.querySelector('h1').innerHTML = infoReplace.city;
             mainCityCard.querySelector('p').innerHTML = infoReplace.weather;
+            mainCityCard.querySelector('.additional-info').innerHTML = infoReplace.additional_info
             btn.querySelector('h2').innerHTML = mainInfo.city;
             btn.querySelector('p').innerHTML = mainInfo.weather;
+            btn.querySelector('.additional-info').innerHTML = mainInfo.additional_info
         });
     });
 }
+
+
--- src/getWeather.js
+++ src/getWeather.js
@@ -1,15 +1,33 @@
 export default function getWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/group?id=524901,498817,1496747,551487,1486209&appid=1693c13db95a0c2b55b2ed680a6b20a6').then(response => response.json()).then(data => {
-        console.log(data)
-        const mainCityCard = document.querySelector('.main-city');    
-        const otherCityCards = document.querySelectorAll('.city-card');
-        mainCityCard.querySelector('h1').innerHTML = data.list[0].name
-        mainCityCard.querySelector('p').innerHTML = `${Math.round(data.list[0].main.temp - 273)}&deg;C, ${data.list[0].weather[0].description}<br><br><img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png" alt="Weather Icon">`;
-        otherCityCards.forEach(elm => {
-            elm.querySelector('h2').innerHTML = data.list[Number(elm.attributes.num.textContent)].name
-            elm.querySelector('p').innerHTML = `${Math.round(data.list[Number(elm.attributes.num.textContent)].main.temp - 273)}&deg;C, ${data.list[Number(elm.attributes.num.textContent)].weather[0].description}<br><br><img src="http://openweathermap.org/img/wn/${data.list[Number(elm.attributes.num.textContent)].weather[0].icon}.png" alt="Weather Icon">`;
-        })
-    })
+    function updateWeather() {
+        fetch('https://api.openweathermap.org/data/2.5/group?id=524901,498817,1496747,551487,1486209&appid=1693c13db95a0c2b55b2ed680a6b20a6')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const mainCityCard = document.querySelector('.main-city');
+                const otherCityCards = document.querySelectorAll('.city-card');
+                mainCityCard.querySelector('h1').innerHTML = data.list[0].name;
+                mainCityCard.querySelector('p').innerHTML = `${Math.round(data.list[0].main.temp - 273)}&deg;C, ${data.list[0].weather[0].description}<br><br><img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png" alt="Weather Icon">`;
+                mainCityCard.querySelector('.feels-like').innerHTML = `<i class="wi wi-alien"></i> Feels like: ${Math.round(data.list[0].main.feels_like - 273)}&deg;C`
+                mainCityCard.querySelector('.humidity').innerHTML = `<i class="wi wi-humidity"></i> Humidity: ${data.list[0].main.humidity}%`;
+                mainCityCard.querySelector('.temp-max').innerHTML = `<i class="wi wi-thermometer"></i> Max Temp: ${Math.round(data.list[0].main.temp_max - 273)}&deg;C`;
+                mainCityCard.querySelector('.temp-min').innerHTML = `<i class="wi wi-thermometer-exterior"></i> Min Temp: ${Math.round(data.list[0].main.temp_min - 273)}&deg;C`;
+                otherCityCards.forEach(elm => {
+                    elm.querySelector('h2').innerHTML = data.list[Number(elm.attributes.num.textContent)].name;
+                    elm.querySelector('p').innerHTML = `${Math.round(data.list[Number(elm.attributes.num.textContent)].main.temp - 273)}&deg;C, ${data.list[Number(elm.attributes.num.textContent)].weather[0].description}<br><br><img src="http://openweathermap.org/img/wn/${data.list[Number(elm.attributes.num.textContent)].weather[0].icon}.png" alt="Weather Icon">`;
+                    elm.querySelector('.additional-info').querySelector('.feels-like').innerHTML = `<i class="wi wi-alien"></i> Feels like: ${Math.round(data.list[Number(elm.attributes.num.textContent)].main.feels_like - 273)}&deg;C`
+                    elm.querySelector('.additional-info').querySelector('.humidity').innerHTML = `<i class="wi wi-humidity"></i> Humidity: ${data.list[Number(elm.attributes.num.textContent)].main.humidity}%`;
+                    elm.querySelector('.additional-info').querySelector('.temp-max').innerHTML = `<i class="wi wi-thermometer"></i> Max Temp: ${Math.round(data.list[Number(elm.attributes.num.textContent)].main.temp_max - 273)}&deg;C`;
+                    elm.querySelector('.additional-info').querySelector('.temp-min').innerHTML = `<i class="wi wi-thermometer-exterior"></i> Min Temp: ${Math.round(data.list[Number(elm.attributes.num.textContent)].main.temp_min - 273)}&deg;C`;
+                });
+            })
+            .catch(error => {
+                console.error('Error when retrieving weather data:', error);
+            });
+    }
+    const interval = 60 * 1000; // 1 минута
+    updateWeather();
+    setTimeout(updateWeather, interval);
 }
 
 
